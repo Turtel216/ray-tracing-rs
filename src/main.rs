76a15590e0efe4903ea0a480b67948b7f84606be
papakonstantinou <papakonstantinou.dm@gmail.com>
2025-11-03 14:53:52 +0200

    Add sphere rendering

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,26 @@ mod color;
 mod ray;
 mod vec;
 
+use std::io;
+
 use color::Color;
 use ray::Ray;
-use std::io;
 use vec::{Point3, Vec3};
 
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = vec::dot(r.direction(), r.direction());
+    let b = 2.0 * vec::dot(oc, r.direction());
+    let c = vec::dot(oc, oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = vec::unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
@@ -33,6 +47,8 @@ fn main() {
         origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     // Render
+
+    print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
         eprint!("\rScanlines remaining: {} ", j);
