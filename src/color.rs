3d051cd3574fd0edc51644e011aaeb355d35e239
papakonstantinou <papakonstantinou.dm@gmail.cm>
2025-11-14 17:14:19 +0200

    Fix shadows

--- src/color.rs
+++ src/color.rs
@@ -12,9 +12,9 @@ pub fn write_color(out: &mut impl Write, pixel_color: Color, samples_per_pixel: 
 
     // Divide the color by the number of samples
     let scale = 1.0 / samples_per_pixel as f32;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = f32::sqrt(scale * r);
+    g = f32::sqrt(scale * g);
+    b = f32::sqrt(scale * b);
 
     writeln!(
         out,
